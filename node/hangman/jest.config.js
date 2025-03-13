9ed9426d1bacd4c597375c502c924d5e2646dc99
Warren MacEvoy <wmacevoy@gmail.com>
2025-03-12 22:37:12 -0600

    move report to tests

--- node/hangman/jest.config.js
+++ node/hangman/jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
     reporters: [
       'default',
       [ 'jest-html-reporter', {
-        outputPath: 'reports/test-report.html',  // Specify the output file path
+        outputPath: 'tests/report.html',  // Specify the output file path
         pageTitle: 'Test Report',                // Title for the HTML page
         // You can add more options here for customization
       } ]
